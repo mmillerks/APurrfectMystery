474e41698c8def0032e72e574e5d7cbe72da4cf2
Mindy Miller <mmillerks@gmail.com>
2022-01-25 19:14:46 -0600

    Third Commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+
+
+class Detective {
+    constructor (name, rating, skill) {
+      this.name = name;
+      this.rating = rating;
+      this.skill = skill;
+    }
+}
+  
+  class Cat {
+    constructor (name, lives) {
+      this.name = name;
+      this.lives = lives;
+    }
+}

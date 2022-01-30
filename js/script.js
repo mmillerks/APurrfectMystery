eb6badd06d01960ea6406b45a90889e476c61c92
Mindy Miller <mmillerks@gmail.com>
2022-01-29 23:47:53 -0600

    SaturdayNight

--- js/script.js
+++ js/script.js
@@ -52,3 +52,43 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+const i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    let elem = document.getElementById("myBar");
+    let width = 1;
+    let id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
+}
+
+const i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    let elem = document.getElementById("catLives");
+    let width = 1;
+    let id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
+}

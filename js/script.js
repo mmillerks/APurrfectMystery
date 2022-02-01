08bb712dea06ce3eff3fb3b1bdf17f7330e157e2
Mindy Miller <mmillerks@gmail.com>
2022-01-31 19:27:45 -0600

    MondayNight1

--- js/script.js
+++ js/script.js
@@ -1,17 +1,33 @@
-class Detective {
-    constructor (name, rating, skill) {
-      this.name = name;
+class Investigator {
+    constructor (rating) {
       this.rating = rating;
-      this.skill = skill;
-    }
+  }
+  updateProgress() {
+    const width = this.rating/6*100;
+    let elem = document.getElementById("myBar");
+    elem.style.width = width + "%";
+    elem.innerHTML = width + "%";
+  }
 }
   
   class Cat {
-    constructor (name, lives) {
-      this.name = name;
+    constructor (lives) {
       this.lives = lives;
     }
+    updateCatProgress() {
+      const catWidth =this.lives/9*100;
+      let update = document.getElementById("myCatBar");
+      update.style.width = catWidth + "%";
+      update.innerHTML = catWidth + "%";
+    }
 }
+
+const player = new Investigator(0);
+
+const sherlock = new Cat(9);
+
+sherlock.updateCatProgress();
+
 
 // Get the modal for the case file
 const modal = document.getElementById("myModal");
@@ -41,20 +57,138 @@ window.onclick = function(event) {
   }
 }
 
-const images = ["./assests/c1.png", "./assets/c2.png"];
-let i = 0;
 
-function goNext() {
-  if (i < 2) {
-    const image = document.getElementById("dynamicImage");
-    image.src = images[i]; 
-    i++;
+const choiceA = document.getElementById("choice1");
+ if (choiceA) {
+   choiceA.onclick = function() {
+     player.rating++;
+     player.updateProgress();
+     sherlock.updateCatProgress();
+   }
   }
-}
 
-const nextButton =  document.getElementById("begin");
+  const choiceB = document.getElementById("choice2");
+  if (choiceB) {
+    choiceB.onclick = function() {
+      player.rating++;
+      player.updateProgress();
+      sherlock.lives--;
+      sherlock.updateCatProgress();
+    }
+   }
 
-nextButton.onclick = goNext;
+   const choiceC = document.getElementById("choice3");
+   if (choiceC) {
+     choiceC.onclick = function() {
+       player.rating++;
+       player.updateProgress();
+       sherlock.lives--;
+       sherlock.updateCatProgress();
+     }
+    }
+
+   const choiceD = document.getElementById("choice4");
+   if (choiceD) {
+     choiceD.onclick = function() {
+       player.rating++;
+       player.updateProgress();
+       sherlock.lives--;
+       sherlock.updateCatProgress();
+     }
+    }
+
+    const choiceE = document.getElementById("choice5");
+    if (choiceE) {
+      choiceE.onclick = function() {
+        player.rating++;
+        player.updateProgress();
+        sherlock.lives--;
+        sherlock.updateCatProgress();
+      }
+     }  
+     const choiceF = document.getElementById("choice6");
+     if (choiceF) {
+     choiceF.onclick = function() {
+       player.rating++;
+       player.updateProgress();
+       sherlock.lives--;
+       sherlock.updateCatProgress();
+     }
+    }
+    const choiceG = document.getElementById("choice7");
+    if (choiceG) {
+     choiceG.onclick = function() {
+       player.rating++;
+       player.updateProgress();
+       sherlock.lives--;
+       sherlock.updateCatProgress();
+     }
+    }
+    const choiceH = document.getElementById("choice8");
+    if (choiceH) {
+      choiceH.onclick = function() {
+        player.rating++;
+        player.updateProgress();
+        sherlock.lives--;
+        sherlock.updateCatProgress();
+      }
+     }
+     const choiceI = document.getElementById("choice9");
+     if (choiceI) {
+       choiceI.onclick = function() {
+         player.rating++;
+         player.updateProgress();
+         sherlock.lives--;
+         sherlock.updateCatProgress();
+       }
+      }
+      const choiceJ = document.getElementById("choice10");
+      if (choiceI) {
+      choiceI.onclick = function() {
+        player.rating++;
+        player.updateProgress();
+        sherlock.lives--;
+        sherlock.updateCatProgress();
+      }
+     }
+     const choiceK = document.getElementById("choice11");
+     if (choiceK) {
+      choiceK.onclick = function() {
+        player.rating++;
+        player.updateProgress();
+        sherlock.lives--;
+        sherlock.updateCatProgress();
+      }
+     }
+     const choiceL = document.getElementById("choice12");
+    if (choiceH) {
+      choiceH.onclick = function() {
+        player.rating++;
+        player.updateProgress();
+        sherlock.lives--;
+        sherlock.updateCatProgress();
+      }
+     }
+
+
+
+
+// const images = ["./assests/c1.png", "./assets/c2.png"];
+// let i = 0;
+
+// function goNext() {
+//   if (i < 2) {
+//     const image = document.getElementById("dynamicImage");
+//     image.src = images[i]; 
+//     i++;
+//   }
+// }
+
+// const nextButton =  document.getElementById("begin");
+
+// nextButton.onclick = goNext;
+
+
 
 // const i = 0;
 // function move() {
@@ -94,3 +228,11 @@ nextButton.onclick = goNext;
 //       }
 //     }
 //   }
+
+
+
+// const width = player.rating/6*100;
+
+// let elem = document.getElementById("myBar");
+// elem.style.width = width + "%";
+// elem.innerHTML = width + "%";
